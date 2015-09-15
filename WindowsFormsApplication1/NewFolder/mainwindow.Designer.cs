cc2398f84f648638f35ac035ad212254b68284f0
amropus <amropussolutions@gmail.com>
2015-09-16 00:46:04 +0530

    change

---
+++ WindowsFormsApplication1/NewFolder/mainwindow.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApplication1.NewFolder
+{
+    partial class mainwindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.masterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transactionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.collectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generateBillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.billDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.collectionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dailyCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monthlyCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yearlyCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changePasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.masterToolStripMenuItem,
+            this.transactionToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(641, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // masterToolStripMenuItem
+            // 
+            this.masterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customerToolStripMenuItem});
+            this.masterToolStripMenuItem.Name = "masterToolStripMenuItem";
+            this.masterToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.masterToolStripMenuItem.Text = "Master";
+            // 
+            // transactionToolStripMenuItem
+            // 
+            this.transactionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.collectionToolStripMenuItem,
+            this.generateBillToolStripMenuItem});
+            this.transactionToolStripMenuItem.Name = "transactionToolStripMenuItem";
+            this.transactionToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.transactionToolStripMenuItem.Text = "Transaction";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchCustomerToolStripMenuItem,
+            this.billDetailsToolStripMenuItem,
+            this.collectionToolStripMenuItem1});
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backUpToolStripMenuItem,
+            this.changePasswordToolStripMenuItem,
+            this.smsToolStripMenuItem});
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // collectionToolStripMenuItem
+            // 
+            this.collectionToolStripMenuItem.Name = "collectionToolStripMenuItem";
+            this.collectionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.collectionToolStripMenuItem.Text = "Collection";
+            // 
+            // generateBillToolStripMenuItem
+            // 
+            this.generateBillToolStripMenuItem.Name = "generateBillToolStripMenuItem";
+            this.generateBillToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.generateBillToolStripMenuItem.Text = "Generate Bill";
+            // 
+            // searchCustomerToolStripMenuItem
+            // 
+            this.searchCustomerToolStripMenuItem.Name = "searchCustomerToolStripMenuItem";
+            this.searchCustomerToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.searchCustomerToolStripMenuItem.Text = "Search Customer";
+            // 
+            // billDetailsToolStripMenuItem
+            // 
+            this.billDetailsToolStripMenuItem.Name = "billDetailsToolStripMenuItem";
+            this.billDetailsToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.billDetailsToolStripMenuItem.Text = "Bill Details";
+            // 
+            // collectionToolStripMenuItem1
+            // 
+            this.collectionToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dailyCollectionToolStripMenuItem,
+            this.monthlyCollectionToolStripMenuItem,
+            this.yearlyCollectionToolStripMenuItem});
+            this.collectionToolStripMenuItem1.Name = "collectionToolStripMenuItem1";
+            this.collectionToolStripMenuItem1.Size = new System.Drawing.Size(164, 22);
+            this.collectionToolStripMenuItem1.Text = "Collection";
+            // 
+            // dailyCollectionToolStripMenuItem
+            // 
+            this.dailyCollectionToolStripMenuItem.Name = "dailyCollectionToolStripMenuItem";
+            this.dailyCollectionToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.dailyCollectionToolStripMenuItem.Text = "Daily Collection";
+            // 
+            // monthlyCollectionToolStripMenuItem
+            // 
+            this.monthlyCollectionToolStripMenuItem.Name = "monthlyCollectionToolStripMenuItem";
+            this.monthlyCollectionToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.monthlyCollectionToolStripMenuItem.Text = "Monthly Collection";
+            // 
+            // yearlyCollectionToolStripMenuItem
+            // 
+            this.yearlyCollectionToolStripMenuItem.Name = "yearlyCollectionToolStripMenuItem";
+            this.yearlyCollectionToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.yearlyCollectionToolStripMenuItem.Text = "Yearly Collection";
+            // 
+            // backUpToolStripMenuItem
+            // 
+            this.backUpToolStripMenuItem.Name = "backUpToolStripMenuItem";
+            this.backUpToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.backUpToolStripMenuItem.Text = "Back up";
+            // 
+            // changePasswordToolStripMenuItem
+            // 
+            this.changePasswordToolStripMenuItem.Name = "changePasswordToolStripMenuItem";
+            this.changePasswordToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.changePasswordToolStripMenuItem.Text = "Change Password";
+            // 
+            // smsToolStripMenuItem
+            // 
+            this.smsToolStripMenuItem.Name = "smsToolStripMenuItem";
+            this.smsToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.smsToolStripMenuItem.Text = "Sms";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // mainwindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainwindow";
+            this.Text = "mainwindow";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem masterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transactionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem collectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generateBillToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem billDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem collectionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dailyCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monthlyCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yearlyCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changePasswordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+
+    }
+}
